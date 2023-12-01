ea8ee4b8b5d555f105b08bb2c6c11acebd8c07ba
omarghandour <alex.forget50@gmail.com>
2023-12-01 06:58:09 +0200

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,43 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-
+import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+      manifest: {
+        name: 'OG',
+        short_name: 'OG',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/public/assets/icons/image.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => {
+              return url.pathname.startsWith('/');
+            },
+            handler: 'NetworkFirst' as const,
+            options: {
+              cacheName: 'home-cache',
+              cacheableResponse: { statuses: [0, 200] },
+            },
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
